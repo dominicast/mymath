4aee2df8a9fc00135af4a9ed9f659a8d78b33a70
Dominic Ast <dominic.ast@bayo.ch>
2019-04-25 22:08:32 +0200

    work in progress

--- magnetpendel_sim_2d/pattern.py
+++ magnetpendel_sim_2d/pattern.py
@@ -5,32 +5,12 @@ from config.triangle import *
 from mp.eq import DiffEq
 from mp.observer import Observer
 import numpy as np
-from threading import Lock
-import threading
+from multiprocessing import Process, Queue, Event
 
 
 # Display path from init_pos with velocity init_vel
 # to one of the magnets
 
-
-cfg = TriangleConfig()
-
-# -- setup plot
-
-fig, ax = plt.subplots()
-
-ax.set_title('Magnetpendel Path')
-
-ax.set_xlabel('x', fontsize=15)
-ax.set_xlim(0, cfg.m_fSimWidth)
-
-ax.set_ylabel('y', fontsize=15)
-ax.set_ylim(0, cfg.m_fSimHeight)
-
-ax.grid(False)
-fig.tight_layout()
-
-# -- simulate equation
 
 class Point:
 
@@ -38,8 +18,6 @@ class Point:
         self._x = x
         self._y = y
         self._color = None
-        self._progressing = False
-        self._printed = False
 
     def get_x(self):
         return self._x
@@ -53,98 +31,92 @@ class Point:
     def get_color(self):
         return self._color
 
-    def set_progressing(self):
-        self._progressing = True
 
-    def get_progressing(self):
-        return self._progressing
+def worker(w_idx, w_cnt, w_points, w_cfg, q, e):
 
-    def set_printed(self):
-        self._printed = True
-
-    def get_printed(self):
-        return self._printed
-
-
-def worker(tid):
-    global points_proc_idx
+    w_point_idx = w_idx
 
     while True:
 
-        my_point = None
-
-        lock.acquire()
-        if points_proc_idx < len(points):
-            my_point = points[points_proc_idx]
-            my_point.set_progressing()
-            points_proc_idx += 1
-        lock.release()
-
-        if my_point is None:
+        if w_point_idx >= len(w_points):
             break
 
-        print('processing ' + repr(my_point.get_x()) + ':' + repr(my_point.get_y()) + ' by thread ' + tid )
+        if e.is_set():
+            break
 
-        init_pos = np.array([my_point.get_x(), my_point.get_y()])
+        w_point = w_points[w_point_idx]
+
+        print('processing ' + repr(w_point.get_x()) + ':' + repr(w_point.get_y()) + ' by worker ' + repr(w_idx))
+
+        init_pos = np.array([w_point.get_x(), w_point.get_y()])
         init_vel = np.array([0, 0])
 
-        integ = it.RungeKutta4th(DiffEq(cfg), init_pos, init_vel)
-        obs = Observer(cfg, None)
-        integ.execute(cfg.m_fTimeStep, cfg.m_nMaxSteps, observer=obs)
+        integ = it.RungeKutta4th(DiffEq(w_cfg), init_pos, init_vel)
+        obs = Observer(w_cfg, None)
+        integ.execute(w_cfg.m_fTimeStep, w_cfg.m_nMaxSteps, observer=obs)
 
         if not obs.get_closest_src() is None:
             clr = obs.get_closest_src().get_color()
         else:
             clr = 'b'
 
-        my_point.set_color(clr)
+        result = Point(w_point.get_x(), w_point.get_y())
+        result.set_color(clr)
 
+        q.put(result)
 
-points = []
-points_proc_idx = 0
-points_print_idx = 0
+        w_point_idx += w_cnt
 
-for x in range(0, cfg.m_fSimWidth, 18):
-    for y in range(0, cfg.m_fSimHeight, 18):
-        points.append(Point(x, y))
-
-lock = Lock()
-
-threads = []
-
-for i in range(0, 5):
-    t = threading.Thread(target=worker, args=('thread'+repr(i),))
-    threads.append(t)
-    t.start()
 
 def animate(i):
-    global points_print_idx
-
-    while True:
-
-        print("!!!!!")
-
-        if points_print_idx >= len(points):
-            return
-
-        point = points[points_print_idx]
-
-        if point.get_color() is None:
-            break
-
-        if point.get_printed():
-            continue
-
+    while not point_queue.empty():
+        point = point_queue.get()
         ax.scatter(point.get_x(), point.get_y(), c=point.get_color(), s=10)
-        point.set_printed()
-
-        points_print_idx += 1
-
-anim = animation.FuncAnimation(fig, animate, interval=500)
 
 
-plt.show()
+if __name__ == '__main__':
 
-# -- display plot
+    cfg = TriangleConfig()
 
+    worker_count = 10
 
+    # -- setup worker
+
+    points = []
+
+    for x in range(0, cfg.m_fSimWidth, 18):
+        for y in range(0, cfg.m_fSimHeight, 18):
+            points.append(Point(x, y))
+
+    point_queue = Queue()
+    terminate_event = Event()
+
+    processes = []
+    for worker_index in range(0, worker_count):
+        process = Process(target=worker, args=(worker_index, worker_count, points, cfg, point_queue, terminate_event))
+        process.start()
+        processes.append(process)
+
+    # -- setup plot
+
+    fig, ax = plt.subplots()
+
+    ax.set_title('Magnetpendel Path')
+
+    ax.set_xlabel('x', fontsize=15)
+    ax.set_xlim(0, cfg.m_fSimWidth)
+
+    ax.set_ylabel('y', fontsize=15)
+    ax.set_ylim(0, cfg.m_fSimHeight)
+
+    ax.grid(False)
+    fig.tight_layout()
+
+    anim = animation.FuncAnimation(fig, animate, interval=500)
+
+    plt.show()
+
+    terminate_event.set()
+
+    for process in processes:
+        process.join()
