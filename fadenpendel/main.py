1f50f9f1663f5af0c4578d9295cc49627d07e466
Dominic Ast <dominic.ast@bayo.ch>
2019-05-23 11:27:51 +0200

    more

--- fadenpendel/main.py
+++ fadenpendel/main.py
@@ -1,4 +1,6 @@
 
+from utils import *
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import juggle_axes
 from matplotlib import animation
@@ -14,90 +16,6 @@ class DiffEq:
 
     def f(self, pos, vel, t):
         return -(9.81/self._radius)*math.sin(pos)
-
-
-class Circle:
-
-    def __init__(self, radius, mp, phi):
-        self._radius = radius
-        self._mp = mp
-        self._phi = phi
-
-    def calc(self, rho):
-        # Kuegelkoordinaten
-
-        x = radius * math.sin(rho) * math.cos(self._phi)
-        y = radius * math.sin(rho) * math.sin(self._phi)
-        z = radius * math.cos(rho)
-
-        v = np.array([x, y, z])
-        v[2] = -v[2]
-        v = self._mp + v
-
-        return v
-
-
-class IntegrationCtx:
-
-    def __init__(self, deq, dt, count):
-        self._deq = deq
-        self._dt = dt
-        self._count = count
-
-    def get_deq(self):
-        return self._deq
-
-    def get_dt(self):
-        return self._dt
-
-    def get_count(self):
-        return self._count
-
-
-class PlotCtx:
-
-    def __init__(self, ax, mp, circle):
-        self._ax = ax
-        self._mp = mp
-        self._circle = circle
-
-        self._mass = None
-        self._line = None
-        self._rr = None
-        self._vel = None
-
-    def get_ax(self):
-        return self._ax
-
-    def get_mp(self):
-        return self._mp
-
-    def get_circle(self):
-        return self._circle
-
-    def get_mass(self):
-        return self._mass
-
-    def set_mass(self, mass):
-        self._mass = mass
-
-    def get_line(self):
-        return self._line
-
-    def set_line(self, line):
-        self._line = line
-
-    def get_rr(self):
-        return self._rr
-
-    def set_rr(self, rr):
-        self._rr = rr
-
-    def get_vel(self):
-        return self._vel
-
-    def set_vel(self, vel):
-        self._vel = vel
 
 
 def animate(i, integ_ctx, plot_ctx):
