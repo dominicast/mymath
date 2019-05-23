5ac3d1ad3cd8404b32c54973afcc35d208cf8ca3
Dominic Ast <dominic.ast@bayo.ch>
2019-05-23 07:55:21 +0200

    more

--- fadenpendel/main.py
+++ fadenpendel/main.py
@@ -9,8 +9,11 @@ import integ as it
 
 class DiffEq:
 
+    def __init__(self, radius):
+        self._radius = radius
+
     def f(self, pos, vel, t):
-        return -9.81*radius*math.sin(pos)
+        return -(9.81/self._radius)*math.sin(pos)
 
 
 class Circle:
@@ -34,14 +37,50 @@ class Circle:
         return v
 
 
-def animate(i):
-    global the_pendulum
+class IntegrationCtx:
 
-    rho,vel = the_pendulum.execute(0.01, 10)
+    def __init__(self, deq, dt, count):
+        self._deq = deq
+        self._dt = dt
+        self._count = count
 
-    pos = cicle.calc(rho)
+    def get_deq(self):
+        return self._deq
+
+    def get_dt(self):
+        return self._dt
+
+    def get_count(self):
+        return self._count
+
+
+class PlotCtx:
+
+    def __init__(self, ax, mp, circle):
+        self._ax = ax
+        self._mp = mp
+        self._circle = circle
+
+    def get_ax(self):
+        return self._ax
+
+    def get_mp(self):
+        return self._mp
+
+    def get_circle(self):
+        return self._circle
+
+
+def animate(i, integ_ctx, plot_ctx):
+
+    rho,vel = integ_ctx.get_deq().execute(integ_ctx.get_dt(), integ_ctx.get_count())
+
+    pos = plot_ctx.get_circle().calc(rho)
 
     # ---
+
+    ax_l = plot_ctx.get_ax()
+    mp_l = plot_ctx.get_mp()
 
     global the_pendulum_mass
     global the_pendulum_line
@@ -50,8 +89,8 @@ def animate(i):
     #global the_pendulum_acc
 
     # line
-    the_pendulum_line.set_data([mp[0], pos[0]], [mp[1], pos[1]])
-    the_pendulum_line.set_3d_properties([mp[2], pos[2]])
+    the_pendulum_line.set_data([mp_l[0], pos[0]], [mp_l[1], pos[1]])
+    the_pendulum_line.set_3d_properties([mp_l[2], pos[2]])
 
     # mass
     the_pendulum_mass.set_data(pos[0], pos[1])
@@ -59,12 +98,12 @@ def animate(i):
 
     # rr
 
-    rr = mp - pos
+    rr = mp_l - pos
 
     if the_pendulum_rr is not None:
         the_pendulum_rr.remove()
 
-    the_pendulum_rr = ax.quiver(pos[0], pos[1], pos[2], rr[0], rr[1], rr[2])
+    the_pendulum_rr = ax_l.quiver(pos[0], pos[1], pos[2], rr[0], rr[1], rr[2])
 
     # velocity
 
@@ -84,7 +123,7 @@ def animate(i):
 
     vv = vv / len * vel * radius
 
-    the_pendulum_vel = ax.quiver(pos[0], pos[1], pos[2], vv[0], vv[1], vv[2])
+    the_pendulum_vel = ax_l.quiver(pos[0], pos[1], pos[2], vv[0], vv[1], vv[2])
 
     # acceleration
 
@@ -97,7 +136,7 @@ def animate(i):
 
     #acc = np.array([x, y, z])
 
-    #the_pendulum_acc = ax.quiver(pos[0], pos[1], pos[2], acc[0], acc[1], acc[2])
+    #the_pendulum_acc = ax_l.quiver(pos[0], pos[1], pos[2], acc[0], acc[1], acc[2])
 
 
 if __name__ == '__main__':
@@ -108,13 +147,30 @@ if __name__ == '__main__':
 
     phi = math.radians(0)
 
+    time_factor = 1
+
+    # -- integration
+
+    deq = it.RungeKutta4th(DiffEq(radius), rho_max, 0)
+
+    integ_dt = 0.01    # [s]
+    integ_count = 10
+    integ_freq = 100   # [ms]
+
+    if integ_dt * integ_count * 1000 != integ_freq:
+        print('Invalid time configuration')
+
+    integ_count = round(integ_count * time_factor)
+
+    integ_ctx = IntegrationCtx(deq, integ_dt, integ_count)
+
     # -- define initial situation
 
     # mount point
     mp = np.array([0, 0, radius+(radius/10)])
 
     # representing the circle of the pendulum
-    cicle = Circle(radius, mp, phi)
+    circle = Circle(radius, mp, phi)
 
     # -- setup plot
 
@@ -139,17 +195,18 @@ if __name__ == '__main__':
     ax.scatter(mp[0], mp[1], mp[2], c='r')
 
     # expected circle
-    pp = cicle.calc(rho_max)
+    pp = circle.calc(rho_max)
     rho_tmp = rho_max - math.radians(5)
     while rho_tmp >= -(rho_max + math.pi / 1000):
-        pn = cicle.calc(rho_tmp)
+        pn = circle.calc(rho_tmp)
         ax.plot(np.array([pp[0], pn[0]]), np.array([pp[1], pn[1]]), np.array([pp[2], pn[2]]), c='k', lw=0.5)
         pp = pn
         rho_tmp -= math.radians(5)
 
     # -- show
 
-    global the_pendulum
+    plot_ctx = PlotCtx(ax, mp, circle)
+
     global the_pendulum_mass
     global the_pendulum_line
     global the_pendulum_rr
@@ -168,8 +225,6 @@ if __name__ == '__main__':
 
     #set_pendulum_pos(sr, 0, rho_max)
 
-    the_pendulum = it.RungeKutta4th(DiffEq(), rho_max, 0)
-
-    anim = animation.FuncAnimation(fig, animate, interval=100, blit=False)
+    anim = animation.FuncAnimation(fig, animate, interval=integ_freq, fargs=(integ_ctx,plot_ctx,), blit=False)
 
     plt.show()
