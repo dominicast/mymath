c332e2327e435c1d12734e7920f16e05ef9b0a70
Dominic Ast <dominic.ast@bayo.ch>
2019-06-07 12:52:01 +0200

    more

--- fadenpendel/force.py
+++ fadenpendel/force.py
@@ -95,7 +95,7 @@ class PendulumMath:
         pos = self._pos
         radius = MathUtils.vec_len(pos - mp)
 
-        #TODO: calculate a vecolity direction
+        # TODO: calculate a vecolity direction
         vel = MathUtils.vec_len(self._vel)
 
         # unit vectors
