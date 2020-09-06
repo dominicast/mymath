041db67bc5e33ee0d08d0ffc7a9c077a486e5d6f
Dominic Ast <dominic.ast@bayo.ch>
2020-09-06 22:24:29 +0200

    gravity_sim_2d examples introduced

--- gravity_sim_2d/main.py
+++ gravity_sim_2d/main.py
@@ -3,6 +3,10 @@ import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
 import integ as it
+from enum import Enum
+
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
 # Solved the following equation
 #
@@ -38,43 +42,92 @@ class DiffEq:
 
             r = b.get_pos() - pos
 
-            #if n % 5 == 0 and trace:
-            #    ax.arrow(pos[0], pos[1], r[0], r[1], fc='y', ec='y', width=0.01, head_width=1, head_length=1, alpha=.05)
-
             m = b.get_size()
             rn = la.norm(r)
             rnq = math.pow( rn, rexp )
 
             fact = G * ( m / rnq )
 
-            #if trace:
-            #    print( repr(m)+' : '+repr( rn )+' : '+repr( rnq )+' : '+repr( fact ) )
-
             result = result + fact * r
-
-        #if n % 5 == 0 and trace:
-        #    vv = 5 * result
-        #    ax.arrow(pos[0], pos[1], vv[0], vv[1], fc='b', ec='b', width=0.01, head_width=1, head_length=1, alpha=.05)
 
         return result
 
-# class DiffEq:
-#
-#     def f( self, pos, vel, t, n ):
-#         result = np.array([0, 0])
-#         for b in bodies:
-#             r = b.get_pos() - pos
-#             if trace:
-#                 ax.arrow(pos[0], pos[1], r[0], r[1], fc='b', ec='b', width=0.01, head_width=1, head_length=1, alpha=.05)
-#             result = result +  0.1 * r
-#         return result
+class Plotter:
+
+    def __init__(self, ax):
+        self._ax = ax
+        self._last_pos = None
+        self._artists = []
+
+    def setup(self):
+        ax.set_title('Path')
+
+        ax.set_xlabel('x', fontsize=15)
+        ax.set_xlim(0, 100)
+
+        ax.set_ylabel('y', fontsize=15)
+        ax.set_ylim(0, 100)
+
+        ax.grid(True)
+
+    def plot_situation(self, init_pos, init_vel, bodies):
+        ax = self._ax
+
+        for b in bodies:
+            ax.scatter(b.get_pos()[0], b.get_pos()[1], s=b.get_size() * 50)
+
+        ax.scatter(init_pos[0], init_pos[1], s=2)
+        ax.arrow(init_pos[0], init_pos[1], init_vel[0], init_vel[1], fc='r', ec='r', width=0.01, head_width=1, head_length=1)
+
+    def plot_frame(self, pos, vel, dv, t):
+        if pos is None:
+            return
+        self._cleanup()
+        self._plot_frame(pos, vel, dv, t)
+
+    def _cleanup(self):
+        for artist in self._artists:
+            artist.remove()
+
+    def _plot_frame(self, pos, vel, dv, t):
+        ax = self._ax
+        artists = []
+
+        # object
+        artist = ax.scatter(pos[0], pos[1], s=2)
+        artist.set_zorder(19)
+        artists.append(artist)
+
+        # path
+        if self._last_pos is not None:
+            lp = self._last_pos
+            ax.plot(np.array([lp[0], pos[0]]), np.array([lp[1], pos[1]]), c='k', lw=0.5)
+
+        # persist
+        self._artists = artists
+        self._last_pos = pos
+
+class Action(Enum):
+    SHOW = 1
+    HTML = 2
+
+
+def animate(i, eq, plotter):
+    pos, vel, dv, t = eq.process_dt( 10 )
+    plotter.plot_frame(pos, vel, dv, t)
 
 
 # -- setup parameters
 
+action = Action.SHOW
+name = 'Two-Bodies'
+frames = None
+if action == Action.HTML:
+    frames = 500
+
 bodies = []
 bodies.append( Body(np.array([20, 60]), 20) )
-bodies.append( Body(np.array([60, 60]), 10) )
+#bodies.append( Body(np.array([60, 60]), 10) )
 
 init_pos = np.array([40,60])
 init_vel = np.array([0,-6.33])
@@ -82,39 +135,32 @@ init_vel = np.array([0,-6.33])
 h = 0.01
 n = 5000
 
-trace = False
-
-# -- setup plot
+# -- setup
 
 fig, ax = plt.subplots()
 
-ax.set_title('Path')
-
-ax.set_xlabel('x', fontsize=15)
-ax.set_xlim(0, 100)
-
-ax.set_ylabel('y', fontsize=15)
-ax.set_ylim(0, 100)
-
-ax.grid(True)
+plotter = Plotter(ax)
+plotter.setup()
 fig.tight_layout()
-
-# -- plot initial situation
-
-for b in bodies:
-    ax.scatter( b.get_pos()[0], b.get_pos()[1], s=b.get_size()*50)
-
-ax.scatter(init_pos[0], init_pos[1], s=2)
-ax.arrow(init_pos[0], init_pos[1], init_vel[0], init_vel[1], fc='r', ec='r', width=0.01, head_width=1, head_length=1)
-
-# -- simulate equation
+plotter.plot_situation(init_pos, init_vel, bodies)
 
 integ = it.RungeKutta4th( DiffEq(), init_pos, init_vel, h )
 
-for i in range(1000):
-    pos_n, vel, dv, t = integ.process_dt( 10 )
-    ax.scatter(pos_n[0], pos_n[1], s=2)
+# -- run
 
-# -- display plot
+anim = animation.FuncAnimation(fig, animate, interval=50, fargs=(integ,plotter,), blit=False, frames=frames)
 
-plt.show()
+if action == Action.SHOW:
+    plt.show()
+
+elif action == Action.HTML:
+
+    print(animation.writers.list())
+
+    Writer = animation.writers['html']
+    writer = Writer(fps=15, metadata=dict(artist='Dominic Ast D!'), bitrate=1800)
+
+    anim.save(name + '.html', writer=writer)
+
+else:
+    raise Exception('Unknown action')
