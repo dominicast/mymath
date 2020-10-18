6b1a4d09fed2e4e33c76617f5238a19a6fc03635
Dominic Ast <dominic.ast@bayo.ch>
2020-10-18 08:40:40 +0200

    more bodies introduced

--- solar_system/config_solar_system.py
+++ solar_system/config_solar_system.py
@@ -23,6 +23,18 @@ class BodiesFactory:
         return VisibleBody(m=mass, r=radius, sp=position, sv=velocity, color=color, size=size, pos_mapper=PositionMapper())
 
     @staticmethod
+    def create_mercury():
+        mass = 3.285*pow(10, 24)
+        radius = 2.439*pow(10, 6)
+        distance = 5.8*pow(10, 10)
+        velocity = 48000
+        position = np.array([[distance, 0., 0.]])
+        velocity = np.array([[0., 0., velocity]])
+        size = (6.96342*pow(10, 8)/100)
+        color = (0., 0., 1.)
+        return VisibleBody(m=mass, r=radius, sp=position, sv=velocity, color=color, size=size, pos_mapper=PositionMapper())
+
+    @staticmethod
     def create_venus():
         mass = 4.867*pow(10, 24)
         radius = 6.0518*pow(10, 6)
@@ -47,13 +59,27 @@ class BodiesFactory:
         return VisibleBody(m=mass, r=radius, sp=position, sv=velocity, color=color, size=size, pos_mapper=PositionMapper())
 
     @staticmethod
+    def create_mars():
+        mass = 6.39*pow(10, 23)
+        radius = 3.3895*pow(10, 6)
+        distance = 2.28*pow(10, 11)
+        velocity = 24070
+        position = np.array([[distance, 0., 0.]])
+        velocity = np.array([[0., 0., velocity]])
+        size = (6.96342*pow(10, 8)/100)
+        color = (0., 0., 1.)
+        return VisibleBody(m=mass, r=radius, sp=position, sv=velocity, color=color, size=size, pos_mapper=PositionMapper())
+
+    @staticmethod
     def create_bodies():
 
         sun = BodiesFactory.create_sun()
+        mercury = BodiesFactory.create_mercury()
         venus = BodiesFactory.create_venus()
         earth = BodiesFactory.create_earth()
+        mars = BodiesFactory.create_mars()
 
-        bodies = [sun, venus, earth]
+        bodies = [sun, mercury, venus, earth, mars]
 
         i = 0
         for body in bodies:
