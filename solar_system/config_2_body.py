4b47cfc414372be1ab7b15ad93a8a80cc784f68b
Dominic Ast <dominic.ast@bayo.ch>
2020-11-03 22:41:56 +0100

    solar system 2-body config introcuded

--- solar_system/config_2_body.py
+++ solar_system/config_2_body.py
@@ -9,22 +9,19 @@ class BodiesFactory:
     def create_u():
         mass = 1
         radius = 1
-        position = np.array([[1., 1., 1.]])
-        velocity = np.array([[0., 0., 0.]])
-        size = 1
+        position = np.array([1., 0., 0.])
+        velocity = np.array([0., 0., 1.])
         color = (1., 0., 0.)
-        return Body(name="u", m=mass, r=radius, sp=position, sv=velocity, color=color, size=size)
+        return Body(name="u", m=mass, r=radius, color=color, pos=position, vel=velocity)
 
     @staticmethod
     def create_v():
         mass = 1
         radius = 1
-        position = np.array([[0., 0., 0.]])
-        velocity = np.array([[0., 1., 0.]])
-        #size = radius
-        size = 1
+        position = np.array([-1., 0., 0.])
+        velocity = np.array([0., 0., -1.])
         color = (0., 0., 1.)
-        return Body(name="v", m=mass, r=radius, sp=position, sv=velocity, color=color, size=size)
+        return Body(name="v", m=mass, r=radius, color=color, pos=position, vel=velocity)
 
     @staticmethod
     def create_bodies():
@@ -33,18 +30,32 @@ class BodiesFactory:
         return [u, v]
 
 
+class Projector:
+
+    @staticmethod
+    def get_pos(body):
+        return body.get_pos()
+
+    @staticmethod
+    def get_size(body):
+        return body.get_r()
+
+
 class Config:
 
     def __init__(self):
-        body_factory = BodiesFactory()
-        self._bodies = body_factory.create_bodies()
-        self._G = 1
-        self._speed = 3
+        self._bodies = BodiesFactory.create_bodies()
+        self._G = 2.5
+        self._speed = 6
         self._dt = 0.001
-        self._azimuth = 0
-        self._elevation = 90
-        self._distance = 80
-        self._show_names = False
+        self._azimuth = 270
+        self._elevation = 20
+        self._distance = 40
+        self._show_names = True
+
+    @staticmethod
+    def create_projector():
+        return Projector()
 
     def get_bodies(self):
         return self._bodies
